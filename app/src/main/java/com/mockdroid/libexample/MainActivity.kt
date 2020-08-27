f2fa314c8ad28beacdf702c4d3ce0cae17e1e4df
0446511018 <0446511018@bussan.co.id>
2020-08-27 17:00:11 +0700

    Testing Lib

--- app/src/main/java/com/mockdroid/libexample/MainActivity.kt
+++ app/src/main/java/com/mockdroid/libexample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mockdroid.libexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mockdroid.lib.LibDebug.Builder.i
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        i("Hello Irfan Nawawi")
     }
 }
